1836646baba5d02d96e7e59ba9a3ab482fb9b294
mh2933 <42110442+mh2933@users.noreply.github.com>
2023-01-03 15:48:45 +0100

    Add files via upload

---
+++ car.c
@@ -0,0 +1,101 @@
+#include "car.h"
+struct Car car1, car2, car3;
+
+void car_init(struct Car* self,
+	          char* brand,
+	          char* model,
+	          char* color,
+	          unsigned int year_of_launch,
+	          enum transmission transmission)
+{
+	self->brand = brand;
+	self->model = model;
+	self->color = color;
+	self->year_of_launch = year_of_launch;
+	self->transmission = transmission;
+
+	
+
+	return;
+}
+
+void car_clear(struct Car* self)
+{
+	self->brand = 0;
+	self->model = 0;
+	self->color = 0;
+	self->year_of_launch = 0;
+	self->transmission = TRANSMISSION_NONE;
+
+	
+	return;
+}
+
+struct Car* car_new(char* brand,
+	                char* model,
+	                char* color,
+	                unsigned int year_of_launch,
+	                enum transmission transmission)
+{
+	struct Car* self = (struct Car*)malloc(sizeof(struct Car));
+	if (!self) return 0;
+	car_init(self, brand, model, color, year_of_launch, transmission);
+	return self;
+}
+
+void car_delete(struct Car** self)
+{
+	free(*self);
+	*self = 0;
+	return;
+}
+
+void car_print(struct Car* self,
+	           FILE* ostream)
+{
+	if (!ostream) ostream = stdout;
+	fprintf(ostream, "--------------------------------------------------------------------------------\n");
+	fprintf(ostream, "Brand: %s\n", self->brand);
+	fprintf(ostream, "Model: %s\n", self->model);
+	fprintf(ostream, "Color: %s\n", self->color);
+	fprintf(ostream, "Year of launch: %u\n", self->year_of_launch);
+	fprintf(ostream, "Transmission: %s\n", car_transmission_str(self));
+	fprintf(ostream, "--------------------------------------------------------------------------------\n\n");
+	return;
+}
+
+/************************************************************************
+* car_transmission_str: Tilldelar Enumerationen transmission till 
+*                       funktionen car_print.
+* 
+*                       - self: Pekare till car objektet.
+************************************************************************/
+char* car_transmission_str(struct Car* self)
+{
+	if (self->transmission == TRANSMISSION_AUTOMATIC) return "Automatic";
+	else if (self->transmission == TRANSMISSION_MANUAL) return "Manual";
+	else if (self->transmission == TRANSMISSION_NONE) return "NONE";
+	else return "Unspecified";
+	
+}
+
+void car_change_color(struct Car* self, char* changed_color, FILE* ostream)
+{
+	
+	if (!ostream) ostream = stdout;
+	fprintf(ostream,"%s\n", self->color = changed_color);
+	return;
+}
+
+/************************************************************************ 
+car_change_transmission: Byter växellådorna till motsatsten
+                         Automatic <---> Manual.
+
+						 - self: Pekare till car objektet.
+************************************************************************/
+char car_change_transmission(struct Car* self, FILE* ostream)
+{
+	if (self->transmission == TRANSMISSION_MANUAL) return TRANSMISSION_AUTOMATIC;
+	if (self->transmission == TRANSMISSION_AUTOMATIC) return TRANSMISSION_MANUAL;
+}
+
---
+++ main.c
@@ -0,0 +1,50 @@
+
+#include "car.h"
+#include <locale.h> // svenska tecken
+
+
+
+/*************************************************************************************************
+ main.c: Lagrar car-data för x antal bilar och skriver ut i terminalen
+         samt till filen "car.txt". Undantag sker om dynamiskt minne inte
+		 kunde allokeras, då avslutas programmet med felkod 1.
+ *************************************************************************************************/
+int main(void)
+{
+	setlocale(LC_ALL, "Swedish"); // svenska tecken
+
+	struct Car car1, car2, car3;
+	FILE* ostream = fopen("car2.txt", "w");
+
+	/* array som används längre ner då ej car_print funktionen används */
+	struct Car* car_array[] =
+	{
+		&car1,
+		&car2,
+		&car3
+	};
+
+	const size_t num_objects = sizeof(car_array) / sizeof(struct Car*);
+
+	/* första initieringen av car objekten */
+	car_init(&car1, "Volvo", "V70", "Black", 1995, TRANSMISSION_MANUAL);
+	car_init(&car2, "BMW", "Z3", "Blue", 2001, TRANSMISSION_AUTOMATIC);
+	car_init(&car3, "Skoda", "Octavia RS", "White", 2003, TRANSMISSION_MANUAL);
+
+	
+	/* andra initieringen, Ändrar växellådan till motsatsen på car1-car3, och sätter utström stdout */
+	car_init(&car1, "Volvo", "V70", "Black", 1995, car_change_transmission(&car1, 0));
+	car_init(&car2, "BMW", "Z3", "Blue", 2001, car_change_transmission(&car2, 0));
+	car_init(&car3, "Skoda", "Octavia RS", "White", 2003, car_change_transmission(&car3, 0));
+	
+	/* Ändrar färgen på car3 till Grön, och output stream anges här som 0 = stdout */
+	car_change_color(&car3, "Green", stdout);
+	
+	/* Array itterator, för de tre struct car objekten, en dubbelpekare behövs då car_array är en enkelpekare */
+	for (struct Car** i = car_array; i < car_array + num_objects; i++)
+	{
+		car_print(*i, stdout);
+	}
+
+	return 0;
+}
