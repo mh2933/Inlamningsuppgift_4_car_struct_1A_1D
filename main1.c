ea10458e4f46e4dbcb618d383708fbe4c95aeb11
mh2933 <42110442+mh2933@users.noreply.github.com>
2023-01-03 15:52:21 +0100

    Rename main.c to main1.c

--- main1.c
+++ main1.c
@@ -5,9 +5,9 @@
 
 
 /*************************************************************************************************
- main.c: Lagrar car-data för x antal bilar och skriver ut i terminalen
+ main.c: Lagrar car-data fÃ¶r x antal bilar och skriver ut i terminalen
          samt till filen "car.txt". Undantag sker om dynamiskt minne inte
-		 kunde allokeras, då avslutas programmet med felkod 1.
+		 kunde allokeras, dÃ¥ avslutas programmet med felkod 1.
  *************************************************************************************************/
 int main(void)
 {
@@ -16,7 +16,7 @@ int main(void)
 	struct Car car1, car2, car3;
 	FILE* ostream = fopen("car2.txt", "w");
 
-	/* array som används längre ner då ej car_print funktionen används */
+	/* array som anvÃ¤nds lÃ¤ngre ner dÃ¥ ej car_print funktionen anvÃ¤nds */
 	struct Car* car_array[] =
 	{
 		&car1,
@@ -26,21 +26,21 @@ int main(void)
 
 	const size_t num_objects = sizeof(car_array) / sizeof(struct Car*);
 
-	/* första initieringen av car objekten */
+	/* fÃ¶rsta initieringen av car objekten */
 	car_init(&car1, "Volvo", "V70", "Black", 1995, TRANSMISSION_MANUAL);
 	car_init(&car2, "BMW", "Z3", "Blue", 2001, TRANSMISSION_AUTOMATIC);
 	car_init(&car3, "Skoda", "Octavia RS", "White", 2003, TRANSMISSION_MANUAL);
 
 	
-	/* andra initieringen, Ändrar växellådan till motsatsen på car1-car3, och sätter utström stdout */
+	/* andra initieringen, Ã„ndrar vÃ¤xellÃ¥dan till motsatsen pÃ¥ car1-car3, och sÃ¤tter utstrÃ¶m stdout */
 	car_init(&car1, "Volvo", "V70", "Black", 1995, car_change_transmission(&car1, 0));
 	car_init(&car2, "BMW", "Z3", "Blue", 2001, car_change_transmission(&car2, 0));
 	car_init(&car3, "Skoda", "Octavia RS", "White", 2003, car_change_transmission(&car3, 0));
 	
-	/* Ändrar färgen på car3 till Grön, och output stream anges här som 0 = stdout */
+	/* Ã„ndrar fÃ¤rgen pÃ¥ car3 till GrÃ¶n, och output stream anges hÃ¤r som 0 = stdout */
 	car_change_color(&car3, "Green", stdout);
 	
-	/* Array itterator, för de tre struct car objekten, en dubbelpekare behövs då car_array är en enkelpekare */
+	/* Array itterator, fÃ¶r de tre struct car objekten, en dubbelpekare behÃ¶vs dÃ¥ car_array Ã¤r en enkelpekare */
 	for (struct Car** i = car_array; i < car_array + num_objects; i++)
 	{
 		car_print(*i, stdout);
