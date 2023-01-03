955f8b2fee01eb468b7102a7defcbb2a7f7f347e
mh2933 <42110442+mh2933@users.noreply.github.com>
2023-01-03 19:14:10 +0100

    Update main1.c

--- main1.c
+++ main1.c
@@ -5,8 +5,10 @@
 
 /*************************************************************************************************
  main.c: Lagrar car-data för x antal bilar och skriver ut i terminalen
-         samt till filen "car.txt". Undantag sker om dynamiskt minne inte
-		 kunde allokeras, då avslutas programmet med felkod 1.
+         samt till filen "car.txt". Nere i for loopen väljs stdout för utskrift till terminalen.
+	 Växellådan på car3 byts till automatic från manual inuti car_intit() och 
+	 car_change_transmission() funktionerna. Färgen byts från White till Green i funktionen
+	 car_change_color.
  *************************************************************************************************/
 int main(void)
 {
@@ -31,13 +33,13 @@ int main(void)
 	car_init(&car3, "Skoda", "Octavia RS", "White", 2003, TRANSMISSION_MANUAL);
 
 	
-	/* andra initieringen, Ändrar växellådan till motsatsen på car1-car3, och sätter utström stdout */
+	/* andra initieringen, Ändrar växellådan till motsatsen på car1-car3 */
 	//car_init(&car1, "Volvo", "V70", "Black", 1995, car_change_transmission(&car1));
 	//car_init(&car2, "BMW", "Z3", "Blue", 2001, car_change_transmission(&car2));
-	//car_init(&car3, "Skoda", "Octavia RS", "White", 2003, car_change_transmission(&car3));
+	car_init(&car3, "Skoda", "Octavia RS", "White", 2003, car_change_transmission(&car3));
 	
 	/* Ändrar färgen på car3 till Grön, och output stream anges här som 0 = stdout */
-	//car_change_color(&car3, "Green");
+	car_change_color(&car3, "Green");
 	
 /************************************************************************
  Array itterator, för de tre struct car objekten, en dubbelpekare behövs 
@@ -45,8 +47,8 @@ int main(void)
  i räknar sedan upp med hjälp av num_objects variablen som tar hjälp av 
  sizeof() operatorn. car_print funktionen används inne i ittetratorn.
 
-	                 - ostream : skriver ut en .txt fil.
-					 - stdout  : skriver ut till terminalen.   
+	                  - ostream : skriver ut en .txt fil.
+			  - stdout  : skriver ut till terminalen.   
 ************************************************************************/
 	for (struct Car** i = car_array; i < car_array + num_objects; i++)
 	{
