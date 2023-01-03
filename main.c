454f09406ac78521cb84b14ad21afbe7dc91c6f6
mh2933 <42110442+mh2933@users.noreply.github.com>
2023-01-03 19:27:36 +0100

    Add files via upload
    Uppgift 1e samt 1d. Bifogar datan till en car.txt fil med hjälp av 
    FILE* ostream = fopen("car.txt", "a+");
    Car datan skrivs först ut i orginalform till ostream sedan ändras car3 objektets färg till Green och växellådan till Automatic.

---
+++ main.c
@@ -0,0 +1,59 @@
+
+#include "car.h"
+#include <locale.h> // svenska tecken
+
+
+
+/*************************************************************************************************
+ main.c: Lagrar car-data för x antal bilar och skriver ut i terminalen
+         samt till filen "car.txt". Undantag sker om dynamiskt minne inte
+		 kunde allokeras, då avslutas programmet med felkod 1.
+ *************************************************************************************************/
+int main(void)
+{
+	setlocale(LC_ALL, "Swedish"); // svenska tecken
+
+	struct Car car1, car2, car3;
+	FILE* ostream = fopen("car4.txt", "a+"); // "r" = read, "w" = write, "a" = append, "r+", "w+", "a+"
+
+	/* array som används längre ner då ej car_print funktionen används */
+	struct Car* car_array[] =
+	{
+		&car1,
+		&car2,
+		&car3
+	};
+
+	const size_t num_objects = sizeof(car_array) / sizeof(struct Car*);
+
+	/* första initieringen av car objekten */
+	car_init(&car1, "Volvo", "V70", "Black", 1995, TRANSMISSION_MANUAL);
+	car_init(&car2, "BMW", "Z3", "Blue", 2001, TRANSMISSION_AUTOMATIC);
+	car_init(&car3, "Skoda", "Octavia RS", "White", 2003, TRANSMISSION_MANUAL);
+
+	
+	/* andra initieringen, Ändrar växellådan till motsatsen på car1-car3, och sätter utström stdout */
+	//car_init(&car1, "Volvo", "V70", "Black", 1995, car_change_transmission(&car1));
+	//car_init(&car2, "BMW", "Z3", "Blue", 2001, car_change_transmission(&car2));
+	car_init(&car3, "Skoda", "Octavia RS", "White", 2003, car_change_transmission(&car3));
+	
+	/* Ändrar färgen på car3 till Grön, och output stream anges här som 0 = stdout */
+	car_change_color(&car3, "Green");
+	
+/************************************************************************
+ Array itterator, för de tre struct car objekten, en dubbelpekare behövs 
+ då car_array är en enkelpekare. Startvärde är första array objektet
+ i räknar sedan upp med hjälp av num_objects variablen som tar hjälp av 
+ sizeof() operatorn. car_print funktionen används inne i ittetratorn.
+
+	                 - ostream : skriver ut en .txt fil.
+					 - stdout  : skriver ut till terminalen.   
+************************************************************************/
+	for (struct Car** i = car_array; i < car_array + num_objects; i++)
+	{
+		car_print(*i, ostream);
+		//printf("%s\n", *i);
+	}
+
+	return 0;
+}
